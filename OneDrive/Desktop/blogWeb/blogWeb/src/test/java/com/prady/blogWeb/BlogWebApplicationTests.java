fc9b1ba03bdb002ab6a02ec6b325dd436cd47848
Prady27 <pradyumnchavan9@gmail.com>
2026-01-29 21:37:49 +0530

    Initial project setup

---
+++ OneDrive/Desktop/blogWeb/blogWeb/src/test/java/com/prady/blogWeb/BlogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prady.blogWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
